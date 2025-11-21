094f6ff8556c86d0a6fa23de6b373a19612d3151
Janendra de silva <your-github-email@example.com>
2025-11-21 18:10:57 +0530

    Initial commit

---
+++ Fast_delievery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fast_delievery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
