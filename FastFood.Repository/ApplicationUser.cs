c0b5f8b1ef50eff45fe3132c6ac9331ffe2e9ef0
Janendra de silva <your-github-email@example.com>
2025-11-22 08:07:40 +0530

    my changes

---
+++ FastFood.Repository/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FastFood.Repository
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ FastFood.Repository/IDbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Repository
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+    }
+}
